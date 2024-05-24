010ce2fa227c046d82136cbe397f88fdf009bcca
robmllze <86869208+robmllze@users.noreply.github.com>
2024-05-24 23:52:35 +1000

    Update

--- lib/src/_all_src.g.dart
+++ lib/src/_all_src.g.dart
@@ -34,7 +34,9 @@ export 'models/public_models/model_organization_pub/model_organization_pub.dart'
 export 'models/public_models/model_project_pub/model_project_pub.dart';
 export 'models/public_models/model_relationship/model_relationship.dart';
 export 'models/public_models/model_user_pub/model_user_pub.dart';
+export 'models/standalone_models/model_cache_information/model_cache_information.dart';
 export 'models/standalone_models/model_connection_permissions/model_connection_permissions.dart';
+export 'models/standalone_models/model_data_collection/model_data_collection.dart';
 export 'models/standalone_models/model_event/model_event.dart';
 export 'models/standalone_models/model_invitation/model_invitation.dart';
 export 'models/standalone_models/model_screen_configuration/model_screen_configuration.dart';
