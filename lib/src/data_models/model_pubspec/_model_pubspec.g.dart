bdb5f9d8af1488527958b96af58cd6854d64fe64
robmllze <86869208+robmllze@users.noreply.github.com>
2024-07-23 23:35:06 +1000

    Update

--- lib/src/data_models/model_pubspec/_model_pubspec.g.dart
+++ lib/src/data_models/model_pubspec/_model_pubspec.g.dart
@@ -199,9 +199,11 @@ class ModelPubspec extends _ModelPubspec {
           )
           .nonNulls
           .nullIfEmpty;
-      final environmentSdk0 = letMap<String, dynamic>(letMap<String, dynamic>(
-        otherData?['environment']?['sdk'],
-      ));
+      final environmentSdk0 = letMap<String, dynamic>(
+        letMap<String, dynamic>(
+          otherData?['environment']?['sdk'],
+        ),
+      );
       final environmentSdk = environmentSdk0?.toString().trim().nullIfEmpty;
       final dependencies0 = otherData?['dependencies'];
       final dependencies = letMap(dependencies0)
@@ -429,8 +431,7 @@ class ModelPubspec extends _ModelPubspec {
   Map<String, dynamic> get dependenciesField => this.dependencies!;
 
   // dependencyOverrides.
-  Map<String, dynamic> get dependencyOverridesField =>
-      this.dependencyOverrides!;
+  Map<String, dynamic> get dependencyOverridesField => this.dependencyOverrides!;
 
   // devDependencies.
   Map<String, dynamic> get devDependenciesField => this.devDependencies!;
